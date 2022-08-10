f281f3050c5916545f5cb3024a661eaf1c741c2a
Sophia Wagner <sophiewagnerla@gmail.com>
2022-08-10 11:02:48 -0700

    Adds new audio files, removes test routes and files

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,12 @@
 from flask import Flask
-# not sure if I need to add these
 import os
 from flask_cors import CORS
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    # Register Blueprints here
-    from app.routes import test_dict_bp
-    app.register_blueprint(test_dict_bp)
-    from app.routes import test_audio_bp
-    app.register_blueprint(test_audio_bp)
-    from app.routes import test_image_bp
-    app.register_blueprint(test_image_bp)
-    from app.routes import test_edited_sound_bp
-    app.register_blueprint(test_edited_sound_bp)
-    from app.routes import test_edited_long_sound_bp
-    app.register_blueprint(test_edited_long_sound_bp)
-    from app.routes import test_edited_medium_sound_bp
-    app.register_blueprint(test_edited_medium_sound_bp)
+
     from app.routes import custom_audio_bp
     app.register_blueprint(custom_audio_bp)
     
-    # not sure if I need this
     CORS(app)
     return app
--- app/routes.py
+++ app/routes.py
@@ -9,77 +9,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #blueprints
-test_dict_bp = Blueprint("test_dict", __name__, url_prefix="/test_dict")
-test_audio_bp = Blueprint("test_audio", __name__, url_prefix="/test_audio")
-test_image_bp = Blueprint("test_image", __name__, url_prefix="/test_image")
-test_edited_sound_bp = Blueprint("test_edited_sound", __name__, url_prefix="/test_edited_sound")
-test_edited_long_sound_bp = Blueprint("test_edited_long_sound", __name__, url_prefix="/test_edited_long_sound")
-test_edited_medium_sound_bp = Blueprint("test_edited_medium_sound", __name__, url_prefix="/test_edited_medium_sound")
 custom_audio_bp = Blueprint("custom_audio", __name__, url_prefix="/custom_audio")
 
-#route that returns a dictionary
-@test_dict_bp.route("", methods = ["GET"])
-def get_test_dict():
-    request_body = request.get_json()
-    test_dict = {"key":"test value"}
-    return jsonify(test_dict)
-
-#route that returns an image
-@test_image_bp.route("", methods = ["GET"])
-def get_test_image():
-    path = os.environ.get("TEST_SOUND_PATH")
-    try:
-        return send_from_directory(path, filename="sample_image.jpg", as_attachment=True)
-    except FileNotFoundError:
-        abort(404)
-
-#route that returns an audio file that already existed
-@test_audio_bp.route("", methods = ["GET"])
-def get_test_audio():
-    path = os.environ.get("TEST_SOUND_PATH")
-    try:
-        return send_from_directory(path, filename="sample_sound.wav", as_attachment=True)
-    except FileNotFoundError:
-        abort(404)
-
-#route that creates and returns a short audio file
-@test_edited_sound_bp.route("", methods = ["GET"])
-def get_test_edited_sound():
-    sound = AudioSegment.from_file("app/sample_sound.wav", format="wav")
-    short = sound[:1000]
-    short.export("app/new_audio.wav", format="wav")
-    path = os.environ.get("TEST_SOUND_PATH")
-    try:
-        return send_from_directory(path, filename="new_audio.wav", as_attachment=True)
-    except FileNotFoundError:
-        abort(404, description ="File not found")
-
-
-#route that creates and returns a long audio file
-@test_edited_long_sound_bp.route("", methods = ["GET"])
-def get_test_edited_long_sound():
-    sound = AudioSegment.from_file("app/sample_sound.wav", format="wav")
-    long = sound * 450
-    long.export("app/new_long_audio.wav", format="wav")
-    # time.sleep(5)
-    path = os.environ.get("TEST_SOUND_PATH")
-    try:
-        return send_from_directory(path, filename="new_long_audio.wav", as_attachment=True)
-    except FileNotFoundError:
-        abort(404, description ="File not found")
-
-#route that creates and returns a 10-minute audio file
-@test_edited_medium_sound_bp.route("", methods = ["GET"])
-def get_test_edited_medium_sound():
-    sound = AudioSegment.from_file("app/sample_sound.wav", format="wav")
-    long = sound * 75
-    long.export("app/new_medium_audio.wav", format="wav")
-    # time.sleep(5)
-    path = os.environ.get("TEST_SOUND_PATH")
-    try:
-        return send_from_directory(path, filename="new_medium_audio.wav", as_attachment=True)
-    except FileNotFoundError:
-        abort(404, description ="File not found")
 
 # from pydub creator https://stackoverflow.com/questions/43408833/how-to-increase-decrease-playback-speed-on-wav-file
 def speed_change(sound, speed=1.0):
@@ -100,12 +31,16 @@ def get_custom_audio():
     request_body = request.args
 
     #getting the root inhale and exhale sounds
-    if request_body["sound"] == "test":
-        inhale = AudioSegment.from_file("app/sample_sound.wav", format="wav")
-        exhale = AudioSegment.from_file("app/sample_sound.wav", format="wav")
+    if request_body["sound"] == "piano":
+        inhale = AudioSegment.from_file("app/piano_rise.wav", format="wav")
+        exhale = AudioSegment.from_file("app/piano_fall.wav", format="wav")
     elif request_body["sound"] == "synth":
         inhale = AudioSegment.from_file("app/synth_rise.wav", format="wav")
         exhale = AudioSegment.from_file("app/synth_fall.wav", format="wav")
+    elif request_body["sound"] == "airy":
+        inhale = AudioSegment.from_file("app/airy_rise.wav", format="wav")
+        exhale = AudioSegment.from_file("app/airy_fall.wav", format="wav")
+
     
     #create the base pattern
     breath_part_lengths = [int(l) for l in request_body["pattern"].split("-")]
